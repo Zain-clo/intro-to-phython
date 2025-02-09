67bc77d9561b93d637fe7e87d2a74c51b2be3966
Zain-clo <zainablawal2013ab@gmail.com>
2025-02-09 08:52:41 -0800

    python intor class activity

---
+++ main.py
@@ -0,0 +1,6 @@
+name = "Zainab"
+Age = 11
+grade = "Grade 8"
+hobby = "cooking and coding"
+school = "The Concord School"
+print("My name is" , name ,  ". I am ",Age , "years old."," I am  in", grade, ". I love",hobby, ". And finally i am from the ", school, "." )
