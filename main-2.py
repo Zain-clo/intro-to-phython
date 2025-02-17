6985e51870d462acaec3a058d11256aba624d4f0
Zain-clo <zainablawal2013ab@gmail.com>
2025-02-16 17:41:48 -0800

    main-2

---
+++ main-2.py
@@ -0,0 +1,20 @@
+Age = 11
+weight = 44.8
+name= ""
+is_student = True
+print(Age)
+Age = float(Age)
+print(Age)
+print(1 +1)
+weight = int(weight)
+print(weight)
+name = bool(name)
+print(name)
+num1 = 20
+num2 = 30
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+print(num1 != num2)
