2d9359eef0d2a584761302e29cef644d37c81575
Zain-clo <zainablawal2013ab@gmail.com>
2025-03-01 17:54:26 -0800

    loops

---
+++ loops.py
@@ -0,0 +1,5 @@
+fruits = ["apples", "banana","watermelon"]
+adj = ["tassty","nice","sweet"]
+for x in adj:
+    for y in fruits:
+        print(x,y)
---
+++ main-4.py
@@ -0,0 +1,22 @@
+for y in range(7):
+    print("Red")
+
+for x in range(1,11):
+   print(x*23)
+for letter in "ZAINAB":
+    print(letter)
+fruits = ["apples", "banana","watermelon"]
+adj = ["tasty","nice","sweet"]
+for x in adj:
+    for y in fruits:
+        print(x,y)
+    sum_of_numbers = sum(range(1,23))
+    print(sum_of_numbers)
+
+for i in range(1,20):
+    for j in range(i):
+        print("*", end="")
+    print()
+
+print("Zainab")
+print("Blessing")
